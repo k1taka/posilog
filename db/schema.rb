f77a399421b3f9a2a94d6745a0a531fb49a22598
k1taka <sofmori@gmail.com>
2019-06-28 18:59:24 +0900

    rename culum type to kind

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_084556) do
+ActiveRecord::Schema.define(version: 2019_06_28_094848) do
 
   create_table "logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "good", null: false
     t.text "chance", null: false
     t.string "iine", default: "0", null: false
-    t.string "type", null: false
+    t.string "kind", null: false
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
