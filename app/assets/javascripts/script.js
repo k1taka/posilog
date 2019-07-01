57dfd4c55864a0b02d546ecd57cc8d3effa64498
k1taka <sofmori@gmail.com>
2019-07-01 19:54:59 +0900

    change can puts console.log

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,11 @@
+// $(function() {
+//   $("btn-primary").on("submit", function() {
+//     console.log("送信ボタンが押されました");
+//   });
+// });
+
+$(function(){
+  $('#exampleFormControlTextarea2').on('change', function(){
+    console.log("送信ボタンが押されました");
+  });
+});
