faa4b2a6b1dde4c5c14561d2324e8c62d73e821a
k1taka <sofmori@gmail.com>
2019-07-02 13:18:17 +0900

    cloud natural language api
    ajax通信を用いて、Googleのcloud natural language apiを用いて、通信をできるようにした。これによって、コメント欄で入力をした文章の自然言語処理が可能になる。

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,12 +1,37 @@
-// $(function() {
-//   $("btn-primary").on("submit", function() {
-//     console.log("送信ボタンが押されました");
-//   });
-// });
+const API_KEY = 'AIzaSyDMqw4Noq1VA4lCC2AeDwqEfbbwiTwoebY';
 
 $(function(){
   $('#exampleFormControlTextarea2').on('change', function(){
     var chance_comment = $(this).prop('value');
     console.log(chance_comment);
+
+    var requestUrl ='https://language.googleapis.com/v1beta1/documents:analyzeSentiment?key=' + API_KEY;
+
+
+    $.ajax({
+      url: requestUrl,
+      type: "POST",
+      contentType : "application/json; charset=utf-8",
+      data        : 
+      JSON.stringify({
+          "document": {
+              "type": "PLAIN_TEXT",
+              "language": "ja",
+              "content": `${chance_comment}`
+          },
+          "encodingType": "UTF8"}),
+    })
+    .done(function(res) {
+      alert("ok");
+      // console.log(res);
+      // var score = $(res).text(documentSentiment.score);
+      // console.log(score);
+      console.log(res.documentSentiment.magnitude);
+
+
+    }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+     alert("error");
+    })
+
   });
 });
