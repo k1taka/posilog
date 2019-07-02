5a0c87bac48f8f39b0ee2ef537e661c29ac93cee
k1taka <sofmori@gmail.com>
2019-07-02 17:39:09 +0900

    Update script.js
    ポジティブな投稿をした場合とネガティブな投稿をした場合とで処理の分岐をさせるようにした。

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,31 +3,33 @@ const API_KEY = 'AIzaSyDMqw4Noq1VA4lCC2AeDwqEfbbwiTwoebY';
 $(function(){
   $('#exampleFormControlTextarea2').on('change', function(){
     var chance_comment = $(this).prop('value');
-    console.log(chance_comment);
-
     var requestUrl ='https://language.googleapis.com/v1beta1/documents:analyzeSentiment?key=' + API_KEY;
-
 
     $.ajax({
       url: requestUrl,
       type: "POST",
       contentType : "application/json; charset=utf-8",
-      data        : 
-      JSON.stringify({
-          "document": {
-              "type": "PLAIN_TEXT",
-              "language": "ja",
-              "content": `${chance_comment}`
-          },
-          "encodingType": "UTF8"}),
-    })
+      data        : JSON.stringify({
+                        "document": {
+                            "type": "PLAIN_TEXT",
+                            "language": "ja",
+                            "content": `${chance_comment}`
+                        },
+                        "encodingType": "UTF8"}),
+                     })
     .done(function(res) {
-      alert("ok");
-      // console.log(res);
-      // var score = $(res).text(documentSentiment.score);
-      // console.log(score);
-      console.log(res.documentSentiment.magnitude);
+      var polarity = res.documentSentiment.polarity
+      var magnitude = res.documentSentiment.magnitude
 
+      console.log(res.documentSentiment.magnitude);
+      console.log(res.documentSentiment.polarity);
+
+      if (polarity >0.3 && magnitude >0.3) {
+          alert("ok");
+      }
+      else{
+          alert("ポジティブな投稿にしましょう");
+      }
 
     }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
      alert("error");
