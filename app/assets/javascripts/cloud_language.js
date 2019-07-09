19cb4ee398941dfdc1c4aa5eaf90e00a8c610593
k1taka <sofmori@gmail.com>
2019-07-10 01:28:18 +0900

    delete unnecessary indent

--- app/assets/javascripts/cloud_language.js
+++ app/assets/javascripts/cloud_language.js
@@ -1,9 +1,7 @@
 $(function(){
-
   function buildHTML(message){
     var html = `<div class="message" data-id= ${message.id}>
                   <div class="message__uper-info">
-
                     <p class="message__text__content">
                       ${content}
                     </p>
@@ -39,16 +37,12 @@ $(function(){
 
       if (polarity >0.3) {
           alert("OK");
-          // $(".btn-chance").text(`positive score:${res.documentSentiment.magnitude*100}`);
-          // $(this).prev().text(`positive score:${res.documentSentiment.polarity}`);
           $(ps).text(`positive score:${res.documentSentiment.polarity}`);
         }
       else{
           alert("ポジティブな投稿にしましょう");
           $(ps).text(`positive score:${res.documentSentiment.polarity}`);
-
       }
-
     }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
      alert("error");
     })
