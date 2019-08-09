54b6ca10e45b8a304e15b76d1e6c6a954ba25b6d
k1taka <sofmori@gmail.com>
2019-08-09 17:33:22 +0900

    Update rename

--- db/migrate/20190809065025_rename_list_ucomments_to_comments.rb
+++ db/migrate/20190809065025_rename_list_ucomments_to_comments.rb
@@ -1,4 +1,4 @@
-class RenameCommentsToComments < ActiveRecord::Migration[5.2]
+class RenameUcommentsToComments < ActiveRecord::Migration[5.2]
   def change
     rename_table :ucomments, :comments
   end
