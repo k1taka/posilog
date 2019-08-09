b8dd58c5d6cc309cbf2264b18377b0c49fd4adff
k1taka <sofmori@gmail.com>
2019-08-09 17:40:59 +0900

    update

--- db/migrate/20190630091356_create_ucomments.rb
+++ db/migrate/20190630091356_create_ucomments.rb
@@ -1,6 +1,6 @@
-class CreateComments < ActiveRecord::Migration[5.2]
+class CreateUcomments < ActiveRecord::Migration[5.2]
   def change
-    create_table :comments do |t|
+    create_table :ucomments do |t|
       t.integer :user_id
       t.integer :log_id
       t.text :text
