6a3975ac4a9f1a3d2fd532bac8b88a83829dfa23
k1taka <sofmori@gmail.com>
2019-08-11 18:06:15 +0900

    store migratino

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_11_081808) do
+ActiveRecord::Schema.define(version: 2019_08_11_083600) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 2019_08_11_081808) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "url", null: false
+    t.string "kind", null: false
+    t.text "profile"
     t.index ["email"], name: "index_stores_on_email", unique: true
     t.index ["reset_password_token"], name: "index_stores_on_reset_password_token", unique: true
   end
