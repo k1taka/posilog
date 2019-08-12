3333b5e5b1614f3a916cfc6a83dc2b867e377a34
k1taka <sofmori@gmail.com>
2019-08-12 16:58:44 +0900

    store registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_user_permitted_parameters, if: :devise_controller?
+  before_action :configure_store_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
+  def configure_user_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:image,:profile])
+  end
+  def configure_store_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:url,:kind,:profile])
   end
 end
