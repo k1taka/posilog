a863ac6726d342645b2005c37db7ecc07a939d36
k1taka <sofmori@gmail.com>
2019-08-19 21:46:21 +0900

    edit customize

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @logs = Log.where(user_id: params[:id]).page(params[:page]).per(5)
-    # @user = @logs[0].user
     @user = User.find(params[:id])
   end
 
