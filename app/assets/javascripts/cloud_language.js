ed8ff91d432d9244660518d911c2c84b60ddc81f
k1taka <sofmori@gmail.com>
2019-08-28 16:34:12 +0900

    cloud language natural APIの修正

--- app/assets/javascripts/cloud_language.js
+++ app/assets/javascripts/cloud_language.js
@@ -10,7 +10,15 @@ $(function(){
     return html;
   }
 
-  $('.form-control-posinega').on('change', function(){
+  function appendGoodScoring(positive_score){
+    $("#exampleFormControlInput_good").val(`${positive_score}`);
+  }
+
+  function appendChanceScoring(positive_score){
+    $("#exampleFormControlInput_chance").val(`${positive_score}`);
+  }
+
+  $('#exampleFormControlTextarea1').on('change', function(){
     var chance_comment = $(this).prop('value');
     var ps = $(this).prev();
     API_KEY = gon.API_KEY;
@@ -32,20 +40,50 @@ $(function(){
     .done(function(res) {
       var polarity = res.documentSentiment.polarity
       var magnitude = res.documentSentiment.magnitude
-
+      var positive_score = 50+50*polarity*magnitude
       console.log(`polarity:${res.documentSentiment.polarity}`);
       console.log(`magnitude:${res.documentSentiment.magnitude}`);
+      $(ps).text(`positive score:${positive_score}`);
 
-      if (polarity >0.3) {
-          alert("OK");
-          $(ps).text(`positive score:${res.documentSentiment.polarity}`);
-        }
-      else{
-          alert("ポジティブな投稿にしましょう");
-          $(ps).text(`positive score:${res.documentSentiment.polarity}`);
-      }
+    appendGoodScoring(positive_score);
+
     }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
      alert("error");
     })
   });
+
+  $('#exampleFormControlTextarea2').on('change', function(){
+    var chance_comment = $(this).prop('value');
+    var ps = $(this).prev();
+    API_KEY = gon.API_KEY;
+
+    var requestUrl ='https://language.googleapis.com/v1beta1/documents:analyzeSentiment?key=' + API_KEY;
+
+    $.ajax({
+      url: requestUrl,
+      type: "POST",
+      contentType : "application/json; charset=utf-8",
+      data        : JSON.stringify({
+                        "document": {
+                            "type": "PLAIN_TEXT",
+                            "language": "ja",
+                            "content": `${chance_comment}`
+                        },
+                        "encodingType": "UTF8"}),
+                     })
+    .done(function(res) {
+      var polarity = res.documentSentiment.polarity
+      var magnitude = res.documentSentiment.magnitude
+      var positive_score = 50+50*polarity*magnitude
+      console.log(`polarity:${res.documentSentiment.polarity}`);
+      console.log(`magnitude:${res.documentSentiment.magnitude}`);
+      $(ps).text(`positive score:${positive_score}`);
+
+      appendChanceScoring(positive_score);
+
+    }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+     alert("error");
+    })
+  });
+
 });
