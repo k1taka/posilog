98ed943bf73d4f0b6064bd0dc0cc5bfce7b2fe4f
k1taka <sofmori@gmail.com>
2019-09-14 19:46:12 +0900

    微修正

--- db/migrate/20190914073449_rename_cuponto_coupon.rb
+++ db/migrate/20190914073449_rename_cuponto_coupon.rb
@@ -1,4 +1,4 @@
-class RenameCoupontoCoupon < ActiveRecord::Migration[5.2]
+class RenameCupontoCoupon < ActiveRecord::Migration[5.2]
   def change
     rename_table :cupons, :coupons #誤ったテーブル名の修正
   end
